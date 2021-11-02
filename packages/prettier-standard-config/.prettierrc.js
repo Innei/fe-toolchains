d8212f70c54cc556ab410524026dd3dd59903f77
Innei <tukon479@gmail.com>
2021-11-02 16:21:41 +0800

    fix: prettier plugin

--- packages/prettier-standard-config/.prettierrc.js
+++ packages/prettier-standard-config/.prettierrc.js
@@ -1,9 +1,10 @@
-module.exports =  {
-  "tabWidth": 2,
-  "printWidth": 80,
-  "semi": false,
-  "singleQuote": true,
-  "trailingComma": "all",
-  "arrowParens": "always",
-  "endOfLine": "lf"
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  plugins: [require.resolve('prettier-plugin-organize-imports')],
 }
