13d5e6e7bcd1536f5999fa97402a9bf73ad815a0
Innei <tukon479@gmail.com>
2022-02-07 19:25:31 +0800

    chore: add `total`

---
+++ packages/tools/index.js
@@ -0,0 +1,9 @@
+const eslintrc_react = require('@innei-util/eslint-config-react-ts')
+const eslintrc_ts = require('@innei-util/eslint-config-ts')
+const prettierrc = require('@innei-util/prettier')
+
+module.exports = {
+  eslintrc_react,
+  eslintrc_ts,
+  prettierrc,
+}
