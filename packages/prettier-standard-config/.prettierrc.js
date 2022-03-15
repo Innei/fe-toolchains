4a92e6adc07e1635a22006482a8b4dce84dc8cab
Innei <tukon479@gmail.com>
2022-03-15 11:08:01 +0800

    chore: add new line for sort

--- packages/prettier-standard-config/.prettierrc.js
+++ packages/prettier-standard-config/.prettierrc.js
@@ -6,6 +6,8 @@ module.exports = {
   trailingComma: 'all',
   arrowParens: 'always',
   endOfLine: 'lf',
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
   plugins: [
     require.resolve('@trivago/prettier-plugin-sort-imports'),
     require.resolve('prettier-plugin-organize-imports'),
