c724c0138b603627cb3d3a2c6e3245fdd57cd932
Innei <i@innei.in>
2023-11-23 23:36:53 +0800

    chore!: remove configs
    Signed-off-by: Innei <i@innei.in>

--- packages/configs/index.js
+++
@@ -1,9 +0,0 @@
-const eslintrc_react = require('@innei/eslint-config-react-ts')
-const eslintrc_ts = require('@innei/eslint-config-ts')
-const prettierrc = require('@innei/prettier')
-
-module.exports = {
-  eslintrc_react,
-  eslintrc_ts,
-  prettierrc,
-}
